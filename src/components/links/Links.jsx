952c73e367d77a4943d745bd0e833670cb6eaf64
KennethKosling <kenkoz7164@gmail.com>
2022-06-09 21:45:44 -0400

    Styled Links and added images instead of text links

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -1,8 +1,12 @@
+import './Links.css'
+import github from '../../images/GitHub.png'
+import linkedIn from '../../images/Linked.png'
+
 export default function Links() {
   return(
-    <div>
-      <a className="linkedIn" href='https://www.linkedin.com/in/kenneth-k-4b2143230/'>LinkedIn</a>
-      <a className="github" href='https://github.com/KennethKosling'>Github</a>
+    <div className='Links'>
+      <a href='https://www.linkedin.com/in/kenneth-k-4b2143230/'><img className="linkedIn" src={linkedIn} alt='LinkedIn' /></a>
+      <a href='https://github.com/KennethKosling'><img className="github" src={github} alt='Github' /></a>
     </div>
   )
 }
