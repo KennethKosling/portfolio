cb3d5be81d641680f4fb0cf926ebfedc827b50d4
KennethKosling <kenkoz7164@gmail.com>
2022-06-10 20:36:50 -0400

    Created Bio and awaiting styling

--- src/components/bio/Bio.jsx
+++ src/components/bio/Bio.jsx
@@ -1,7 +1,13 @@
+import Me from '../../images/Me.jpg'
+
 export default function Bio() {
   return(
     <div>
-      <h1>Bio</h1>
+      <h1>Kenneth Kosling</h1>
+      <img className='headshot' src={Me} alt="Kenneth Kosling" />
+      <div>
+        <h3>I am a software engineer with major interests in gaming and technology. Check out these projects I've made recently!</h3>
+      </div>
     </div>
   )
 }
