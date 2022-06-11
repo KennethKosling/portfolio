12d4786cf13b6c3f695ee6d4006faef33700e3fd
KennethKosling <kenkoz7164@gmail.com>
2022-06-10 20:44:47 -0400

    Styled Bio component

--- src/components/bio/Bio.jsx
+++ src/components/bio/Bio.jsx
@@ -1,12 +1,13 @@
+import './Bio.css'
 import Me from '../../images/Me.jpg'
 
 export default function Bio() {
   return(
-    <div>
-      <h1>Kenneth Kosling</h1>
+    <div className='Bio'>
+      <h1>Hi! My name is Kenneth Kosling.</h1>
       <img className='headshot' src={Me} alt="Kenneth Kosling" />
       <div>
-        <h3>I am a software engineer with major interests in gaming and technology. Check out these projects I've made recently!</h3>
+        <h3>I'm a software engineer with major interests in gaming and technology. Check out these projects I've made recently!</h3>
       </div>
     </div>
   )
